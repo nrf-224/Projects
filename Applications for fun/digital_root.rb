6c2a9a4eb0e9ab702bc0ed50e71f55d127bb0acc
Николай Колосов < >
2016-12-14 21:28:29 +0300

    Created a program, which calculate digital root

---
+++ Applications for fun/digital_root.rb
@@ -0,0 +1,24 @@
+def digital_root(n)
+loop do
+	n = n.to_s.split('').map(&:to_i)
+	n = n.inject(0){ |result, elem| result + elem }
+
+	if n < 10
+	break
+	end
+end
+
+puts n
+end
+
+
+puts "Enter digit to calculate digital root:"
+n = gets.chomp
+digital_root(n)
+
+
+
+
+
+
+
