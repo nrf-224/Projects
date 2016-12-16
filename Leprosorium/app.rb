764895f0e8daddc8ee2603b435c8d415cd3fc966
Николай Колосов < >
2016-12-17 00:13:16 +0300

    Added post title and comments to the code

--- Leprosorium/app.rb
+++ Leprosorium/app.rb
@@ -1,27 +1,30 @@
 #encoding: utf-8
-require 'rubygems'
-require 'sinatra'
-require 'sinatra/reloader'
-require 'sqlite3'
+require 'rubygems' #вызов гема rubygems
+require 'sinatra' #вызов гема sinatra
+require 'sinatra/reloader' #вызов егма sinatra reloader
+require 'sqlite3' #вызов гема sqlite3
 
-def init_db
-	@db = SQLite3::Database.new 'leprosorium.db'
-	@db.results_as_hash = true
-end
+def init_db #определяем метод init_db, при помощи которого будет создаваться база данных при деплое приложения
+	@db = SQLite3::Database.new 'leprosorium.db' #определяем глобальную переменную db и присваиваем ей базу leprosorium.db через модули и классы sqlite3
+	@db.results_as_hash = true #определяем параметр, благодаря которому данные из таблицы будут возвращены в виде хеш-таблицы, а не массива
+end #закрытие метода init_db
 
-before do
+before do #вызываем метод init_db перед обращением к любому из url
 	init_db
 end
 
-configure do
-	init_db
+configure do #определяем код, который будет выполняться каждый раз при старте Sinatra
+	init_db #вызов метода init_db
+	#выполняем sql-запрос к базе данных leprosorium.db через переменную @db
+	#запрос проверяет наличие в базе таблицы Posts, и если её нет, то создаёт. В таблице есть поля Id (первичный ключ), Date (дата поста), Text (содержание поста), Title (Заголовок)
 	@db.execute 'CREATE  TABLE IF NOT EXISTS Posts
 	(
 		Id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, 
 		created_date DATE, 
-		content TEXT
+		content TEXT,
+		title TEXT
 	)'
-
+	#запрос проверяет наличие в базе таблицы Comments, и если её нет, то создаёт. В таблице есть поля Id (первичный ключ), Date (дата поста), Text (содержание поста), post_id (номер поста)
 	@db.execute 'CREATE  TABLE IF NOT EXISTS Comments
 	(
 		Id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, 
@@ -29,30 +32,32 @@ configure do
 		content TEXT,
 		post_id integer
 	)'
-end
+end #закрытие configure
 
-get '/' do
+get '/' do #определяем метод get, выполняемый при обращении к главной странице сайта
+#передаём в глобальную переменную results результат выполнения sql-запроса к базе данных
+#запрос выбирает все строки из таблицы Posts и делает обратную сортировку по полю id
 @results = @db.execute 'select * from Posts order by id desc'
 
-erb :index	
+erb :index	#выводим на экран страницу index.erb
 end
 
-get '/new' do
-erb :new
+get '/new' do #определяем метод get, выполняемый при обращении к главной странице сайта 
+erb :new	#выводим на экран страницу new.erb
 end
 
-post '/new' do
+post '/new' do #определяем метод post, выполняемый при отправке post-запроса к странице /new
 	@content  = params[:content]
-	if @content.length <= 0
+	@title = params[:title]
+	if @content.length <= 0 
 		@error = "Введите текст поста"
 		return erb :new
 	end
 
-	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [@content]
+	@db.execute 'insert into Posts (content, created_date, title) values (?, datetime(), ?)', [@content, @title]
 
 	redirect to '/'
 
-	erb "You typed #{@content}"
 
 end
 
