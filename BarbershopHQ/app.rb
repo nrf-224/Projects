a6c1c3c8525082cb705a02a03e931186084f7bd8
Николай Колосов < >
2016-12-17 17:01:45 +0300

    Downloaded default version of Barbershop HQ

--- BarbershopHQ/app.rb
+++ BarbershopHQ/app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db" #создаётся подключение к базе данных barbershop.db
+#символ :database является служебным
+
+class Client < ActiveRecord::Base #создана сущность client, которая наследуется от сущности Base модуля ActiveRecord
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
