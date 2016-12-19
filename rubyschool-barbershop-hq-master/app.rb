67c7eef3546f0a3e76a6948adc437c2468bac29e
Николай Колосов < >
2016-12-20 00:14:56 +0300

    Added comments

--- rubyschool-barbershop-hq-master/app.rb
+++ rubyschool-barbershop-hq-master/app.rb
@@ -6,7 +6,8 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true, length: { minimum: 3 }
+	validates :name, presence: true, length: { minimum: 3 } #метод validates проводит валидацию данных, передаваемых в базу данных. в данном случае проверка, что поле заполнено и его длина минимум 3 символа
+	# в метод validates передаётся хеш. в данном случае presence => true, length => {minimum => 3}
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true	
@@ -24,14 +25,14 @@ get '/' do
 end
 
 get '/visit' do
-	@c = Client.new
+	@c = Client.new #создан экземпляр сущности client для того, чтобы на странице visit сохранялись введённые, но ещё не отправленные поля формы
 	erb :visit
 end
 
 post '/visit' do
 
-	@c = Client.new params[:client]
-	if @c.save
+	@c = Client.new params[:client] #создаём экземпляр класса Client и передаём ему значение параметров client
+	if @c.save #метод save проводит валидацию при сохранении, и возвращает значения true или false. в данном случае подразумевается if @c.save == true
 		erb "<h2>Спасибо, вы записались!</h2>"
 	else
 		@error = @c.errors.full_messages.first
