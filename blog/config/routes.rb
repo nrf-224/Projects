15bdb167f025894fc75cba5d854f7f6953a03a18
Николай Колосов < >
2017-01-02 11:13:26 +0300

    Add new contact form

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
 
-  resource :contacts, only: [:new, :create] #создаём маршрут к странице contacts и указываем, что будем использовать только REST-методы new и create
+  resource :contacts, only: [:new, :create] #создаём маршрут к странице contacts и указываем, что будем использовать только REST-методы new (get) и create (post)
   resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
