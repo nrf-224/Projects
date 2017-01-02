96084ad8a723e3999318cb1bbd19dbd001b9f64f
Николай Колосов < >
2017-01-03 00:06:26 +0300

    Add terms and about pages

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   #get 'contacts' => 'contacts#new'
   #resource :contacts, only: [:create] 
   resource :contacts, only: [:new, :create], path_names: { :new => '' } #создаём маршрут к странице contacts и указываем, что будем использовать только REST-методы new (get) и create (post), а так же путь к new
