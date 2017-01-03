b3d9383df802102ebac92bbb775514f9f30034e3
Николай Колосов < >
2017-01-03 13:10:03 +0300

    Add comments

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   #get 'contacts' => 'contacts#new'
   #resource :contacts, only: [:create] 
   resource :contacts, only: [:new, :create], path_names: { :new => '' } #создаём маршрут к странице contacts и указываем, что будем использовать только REST-методы new (get) и create (post), а так же путь к new
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
