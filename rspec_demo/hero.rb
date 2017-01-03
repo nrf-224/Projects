d5ffcf1fc77343b6567efc2aff66ec8d93928ee8
Николай Колосов < >
2017-01-03 17:06:06 +0300

    Add hero entity

--- rspec_demo/hero.rb
+++ rspec_demo/hero.rb
@@ -1,2 +1,27 @@
 class Hero
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name
+    @name
+  end
+
+  def power_up
+    @health += 10
+  end
+
+  def power_down
+    @health -=10
+  end
+
+  def hero_info
+    "#{@name} has #{@health} healh"
+  end
 end
+
+hero = Hero.new 'mike'
+puts hero.name
+
+puts hero.hero_info
