9750f5e6042a3f06964a576254c530e6a2ba8605
Николай Колосов < >
2017-01-06 18:06:48 +0300

    Add cart functionality

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ApplicationRecord #файл модели сущности Produc
   Product.order(:updated_at).last
   end
   
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+  end
 end
