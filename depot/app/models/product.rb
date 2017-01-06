4d8bf68402e07eabeb74f23f33cd3550887851f8
Николай Колосов < >
2017-01-06 15:02:38 +0300

    Add client view functionality

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord #файл модели сущности Produc
   validates :price, numericality: {greater_than_or_equal_to: 0.01} #валидация цены, передаваемой в базу данных. Критерий валидации - цена больше или равна 0.01
   validates :title, uniqueness: true #валидация того, что параметр title более не встречается в базе данных
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'URL должен указывать на изображение формата GIF, JPG или PNG.' }
+
+  def self.latest
+  Product.order(:updated_at).last
+  end
+  
 end
