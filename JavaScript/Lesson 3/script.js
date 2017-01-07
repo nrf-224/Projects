c07589d7ce6139590cf072a4477925d62408666e
Николай Колосов < >
2017-01-07 23:05:00 +0300

    Lesson 3

--- JavaScript/Lesson 3/script.js
+++ JavaScript/Lesson 3/script.js
@@ -1,13 +1,58 @@
-var name; //определение одной переменной
-var b, c; //определение нескольких переменных при помощи одного оператора var
+//var name; //определение одной переменной
+//var b, c; //определение нескольких переменных при помощи одного оператора var
 
-name = 10; //присвоение значения переменной
-b ='Hello';
-var a = "str";
-
+//name = 10; //присвоение значения переменной
+//b ='Hello';
+//var a = "str";
+/*
 alert(name); //вывод на экран значения переменной
 alert(b);
 
 alert(name);
 name = "str2";
 alert(name);
+*/
+//null type
+//name = null;
+//alert (typeof(name)); //выводим на экран тип переменной name
+
+//undefined type
+//var test;
+//alert (test);
+
+//NUMBER TYPE
+//10
+//2.34
+//3.14
+
+//alert(0xFF);
+/*
+var numb = 10.5;
+alert(typeof(numb));
+*/
+
+//var a,b,c;
+//a = 5;
+//b = 10;
+//c = a + b
+//c = a - b
+//c = a * b
+//c = a / b
+//c = a % b
+//alert(c);
+
+//var str = "<h1>строка\nстрока\nстрока</h1>"; //перевод строк при помощи \n (как в Ruby)
+//alert(str);
+//document.write(str); //вывод значения переменной str непосредственно на веб-страницу. весь html-код в переменной будет корректно интерпретирован браузером
+
+//var test = 2 * '10';
+//alert(test);
+
+var x = 10;
+var y = 12.5;
+var z = '40';
+y = parseFloat(y);
+z = parseInt(z);
+result = x + y + z;
+
+document.write(result);
