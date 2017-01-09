8912717bf96a064b0279a2fbd7a020d4332f076f
Николай Колосов < >
2017-01-09 23:27:32 +0300

    Add product validations and comments

--- depot2/app/models/product.rb
+++ depot2/app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true #добавляем валидацию, которая происходит при сохранении данных в таблицу Products
+  #данная валидация проверяет, что поля title, description и image_url не пусты при нажатии кнопки Create Product
 end
