25707bf3ecfe6a888b6931fe7540fdcca8916d56
Николай Колосов < >
2017-01-12 21:32:47 +0300

    Move shopping cart code to the partials, fix layout application.html.erb

--- depot2/app/controllers/store_controller.rb
+++ depot2/app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart #добавляем модуль CurrentCart из папки app\controllers\concerns
+  before_action :set_cart #вызываем текущее значение переменной set_cart из файла app\controllers\carts_controller.rb
   def index
     @products = Product.order(:title) #определяем переменную класса @products, в которую передаём все экземпляры сущности Product, отсортированные по значению поля title
   end
