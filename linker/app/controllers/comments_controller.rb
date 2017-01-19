25eee892ead6eb3ec2c2b383810f931fb19eae7c
Николай Колосов < >
2017-01-19 23:14:52 +0300

    Minor fixes. Comments still anonymous

--- linker/app/controllers/comments_controller.rb
+++ linker/app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
   def create
     @link = Link.find(params[:link_id])
     @comment = @link.comments.create(comment_params)
+    @comment.user_id = current_user
     redirect_to link_path(@link)
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :body)
+      params.require(:comment).permit(:body)
     end
 end
